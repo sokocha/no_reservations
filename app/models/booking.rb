36453992acec19035dc2b363783f0fb87189e33e
Sadiq Okocha <sokocha@gmail.com>
2015-04-19 08:01:37 +0100

    validates a party has a min of 1 and max of 7

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,5 @@ class Booking < ActiveRecord::Base
   belongs_to :restaurant
   validates :user_id, presence: true
   validates :restaurant_id, presence: true
+  validates :party, presence: true
 end
