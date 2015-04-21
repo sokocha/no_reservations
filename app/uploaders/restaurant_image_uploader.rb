599c6933065092922ac656b2538780f83ceeb880
Sadiq Okocha <sokocha@gmail.com>
2015-04-21 15:43:35 +0100

    sets a max size for restaurant image

--- app/uploaders/restaurant_image_uploader.rb
+++ app/uploaders/restaurant_image_uploader.rb
@@ -36,9 +36,9 @@ class RestaurantImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [100, 100]
   end
 
-  # version :detail do
-  #   process :resize_to_fit => [400, 400]
-  # end
+  version :detail do
+    process :resize_to_fit => [400, 400]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
